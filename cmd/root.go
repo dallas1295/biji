71782c16442c03545b1bd19dd8cfbeedb19ba96a
Dallas <andrewdallassherman@gmail.com>
2025-12-11 11:06:19 +0800

    Adding and scaffoldign Cobra CLI environment

---
+++ cmd/root.go
@@ -0,0 +1,43 @@
+// Package cmd is for use with the Cobra CLI tool to develop a CLI interface for biji notes.
+// it will include most if not all funcitonality of main GUI application.
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "biji",
+	Short: "biji is a note taking application designed for both TUI and GUI environments.",
+	Long: `biji is a not taking application designed for both TUI and GUI environments.
+		It's being used to learn Go lang for both scripting and normal  app development;
+		It will eventually grow into having Cloud Sync and storage and an Android application.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.biji.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
--- main.go
+++ main.go
@@ -1,30 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-	"github.com/dallas1295/biji/local"
-)
-
-// TODO: Need to move app into a seperate fiile if possible
+import "github.com/dallas1295/biji/cmd"
 
 func main() {
-	store := &local.Store{}
-	store.Init()
-
-	a := app.New()
-	w := a.NewWindow("Biji Notes")
-
-	nameBox := widget.NewEntry()
-	contentBox := widget.NewEntry()
-	button := widget.NewButton("Create Note", func() {
-		go store.AddNote(nameBox.Text, contentBox.Text)
-	})
-
-	label := widget.NewLabel("Hello Biji!")
-	content := container.NewVBox(button, label, container.NewHBox(nameBox, contentBox))
-
-	w.SetContent(content)
-	w.ShowAndRun()
+	cmd.Execute()
 }
