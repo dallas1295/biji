c50fe771d13246b03e564f954178b3f1fd3d14e8
Dallas S. <andrewdallassherman@gmail.com>
2025-12-20 14:48:03 +0800

    feat: Export functionality, multi argument deletion and export, migration
    export: explicit notes provided into Documents directory.
    migrate: exports all notes to a zip file in Documents directory

--- cmd/notes.go
+++ cmd/notes.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"codeberg.org/dallas1295/biji/local"
@@ -11,18 +13,40 @@ import (
 
 func newNote(s *local.Store) *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "new [name] [content]",
+		Use:   "new [name]",
 		Short: "Create a new note",
-		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			name := args[0]
-			content := args[1]
-			_, err := s.AddNote(name, content)
+			var name, content string
+			reader := bufio.NewReader(os.Stdin)
+
+			if len(args) > 0 {
+				name = args[0]
+			} else if len(args) > 1 {
+				log.Fatalf("Too many arguments")
+			} else {
+				fmt.Print("Name: ")
+
+				rawInput, err := reader.ReadString('\n')
+				if err != nil {
+					log.Fatalf("Failed to read name input: %v", err)
+				}
+				name = strings.TrimSpace(rawInput)
+			}
+
+			fmt.Print("Content: ")
+			rawInput, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatalf("Failed to read name input: %v", err)
+			}
+			content = strings.ReplaceAll(rawInput, "\\n ", "\n")
+			content = strings.TrimSpace(content)
+
+			_, err = s.AddNote(name, content)
 			if err != nil {
 				log.Fatalf("Note creation failed: %v", err)
 			}
 
-			fmt.Println("note created sucessfully")
+			fmt.Println("Note created sucessfully")
 
 			return nil
 		},
@@ -33,21 +57,27 @@ func newNote(s *local.Store) *cobra.Command {
 
 func deleteNote(s *local.Store) *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "delete [name]",
+		Use:   "delete [name], [name], ...",
 		Short: "Delete a note by name",
-		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			name := args[0]
-			id, err := s.FindNoteID(s.Notes, name)
-			if err != nil {
-				log.Fatalf("Could not find note: %v", err)
+			if len(args) == 0 {
+				fmt.Println("No note names provided")
+				return nil
+			}
+			for i := range args {
+				name := strings.TrimSpace(args[i])
+				id, err := s.FindNoteID(s.Notes, name)
+				if err != nil {
+					log.Fatalf("Error deleting note: %v", err)
+				}
+
+				err = s.DeleteNote(id)
+				if err != nil {
+					log.Fatalf("Error deleting note: %v", err)
+				}
 			}
 
-			err = s.DeleteNote(id)
-			if err != nil {
-				log.Fatalf("Note deletion failed: %v", err)
-			}
-			fmt.Println("note deleted successfuly")
+			fmt.Println("Note deleted successfully")
 
 			return nil
 		},
@@ -58,12 +88,23 @@ func deleteNote(s *local.Store) *cobra.Command {
 
 func updateNoteName(s *local.Store) *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "update name [currName] [newName]",
-		Short: "Update name of note",
-		Args:  cobra.ExactArgs(3),
+		Use:   "rename [currName] [newName]",
+		Short: "rename note with current name [name]",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			currName := strings.TrimSpace(args[1])
-			newName := strings.TrimSpace(args[2])
+			reader := bufio.NewReader(os.Stdin)
+			currName := strings.TrimSpace(args[0])
+			var newName string
+
+			if len(args) > 2 {
+				newName = strings.TrimSpace(args[1])
+			} else {
+				fmt.Print("New Name: ")
+				rawInput, err := reader.ReadString('\n')
+				if err != nil {
+					log.Fatalf("Failed to read new name input: %v", err)
+				}
+				newName = strings.TrimSpace(rawInput)
+			}
 
 			id, err := s.FindNoteID(s.Notes, currName)
 			if err != nil {
@@ -72,9 +113,109 @@ func updateNoteName(s *local.Store) *cobra.Command {
 
 			_, err = s.UpdateNoteName(id, newName)
 			if err != nil {
-				log.Fatalf("failed to update note name: %v", err)
+				log.Fatalf("Failed to update note name: %v", err)
 			}
 
+			return nil
+		},
+	}
+
+	return &cmd
+}
+
+func viewNote(s *local.Store) *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "view [name]",
+		Short: "View note contents",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			name := strings.TrimSpace(args[0])
+
+			id, err := s.FindNoteID(s.Notes, name)
+			if err != nil {
+				log.Fatalf("Could not find note: %v", err)
+			}
+
+			note, err := s.GetNoteFromID(id)
+			if err != nil {
+				log.Fatalf("Failed to retrieve note: %v", err)
+			}
+
+			fmt.Printf(
+				"\n\nNote: %s\nContent: %s\n\n",
+				note.Name,
+				note.Content,
+			)
+
+			return nil
+		},
+	}
+
+	return &cmd
+}
+
+func listNotes(s *local.Store) *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "list",
+		Short: "list currently saved notes",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			names := s.GetNoteNames()
+			if len(names) == 0 {
+				fmt.Printf("	no notes\n")
+				return nil
+			}
+
+			fmt.Println("Notes:")
+			for _, name := range names {
+				fmt.Printf("	%s\n", name)
+			}
+
+			return nil
+		},
+	}
+
+	return &cmd
+}
+
+func export(s *local.Store) *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "export [name] [name] ...",
+		Short: "Export designated note(s) to .md file",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				fmt.Println("No note names provided")
+				return nil
+			}
+			for i := range args {
+				name := strings.TrimSpace(args[i])
+				id, err := s.FindNoteID(s.Notes, name)
+				if err != nil {
+					log.Fatalf("Error exporting note: %v", err)
+				}
+
+				err = s.ExportNote(id)
+				if err != nil {
+					log.Fatalf("Error exporting note: %v", err)
+				}
+
+				fmt.Printf("%s successfully exported\n", name)
+			}
+
+			return nil
+		},
+	}
+
+	return &cmd
+}
+
+func migrate(s *local.Store) *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "migrate",
+		Short: "Export all notes for migration",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := s.ExportAll()
+			if err != nil {
+				return fmt.Errorf("error exporting notes: %v", err)
+			}
 			return nil
 		},
 	}
--- cmd/root.go
+++ cmd/root.go
@@ -3,19 +3,20 @@
 package cmd
 
 import (
+	"log"
 	"os"
 
 	"codeberg.org/dallas1295/biji/local"
+	"codeberg.org/dallas1295/biji/tui"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use: "biji",
-	// Short: "biji is a note taking application designed for both TUI and GUI environments.",
-	// Long: `biji is a not taking application designed for both TUI and GUI environments.
-	// 	It's being used to learn Go lang for both scripting and normal  app development;
-	// 	It will eventually grow into having Cloud Sync and storage and an Android application.`,
+	Use:   "biji",
+	Short: "biji is a note taking application designed for both TUI and GUI environments.",
+	Long: `biji is a not taking application designed for both TUI and GUI environments.
+		It's being used to learn Go lang for both scripting and normal app development;`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -25,6 +26,19 @@ func Execute(s *local.Store) {
 	rootCmd.AddCommand(newNote(s))
 	rootCmd.AddCommand(deleteNote(s))
 	rootCmd.AddCommand(updateNoteName(s))
+	rootCmd.AddCommand(listNotes(s))
+	rootCmd.AddCommand(viewNote(s))
+	rootCmd.AddCommand(export(s))
+	rootCmd.AddCommand(migrate(s))
+
+	// Defining absent subcommands to launch the tui environment.
+
+	rootCmd.Run = func(cmd *cobra.Command, args []string) {
+		// initialize tui
+		if err := tui.Run(s); err != nil {
+			log.Fatalf("TUI exited with err: %v", err)
+		}
+	}
 
 	err := rootCmd.Execute()
 	if err != nil {
